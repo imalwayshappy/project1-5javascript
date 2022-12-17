3a989db5e5c13c3e7898b69749ff600207ae1e85
imalwayshappy <108452627+imalwayshappy@users.noreply.github.com>
2022-12-16 22:52:41 -0800

    Update reservation.js
    update validation text errors

--- reservation.js
+++ reservation.js
@@ -3,57 +3,49 @@ $(document).ready( () => {
 	const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 	//focus to the "arrival date" text box
 	$("#arrival_date").focus();
-	//event handler for submit event
-	$("#reservation_form").submit(function(evt){
-		
-		var validation = 0;
-		if($("#arrival_date").val() == "")
-		{
-			alert("Please enter the arrival date")
-			validation =1;
-			evt.preventDefault();
-		}
-		if($("#nights").val() == "")
-		{
-			alert("Please enter the nights ")
-			validation =1;
-			evt.preventDefault();
-		}
-		if($("#name").val() == "")
-		{
-			alert("Please enter your name")
-			validation =1;
-			evt.preventDefault();
-		}
-		if($("#email").val() == "")
-		{
-			alert("Please enter your email")
-			validation =1;
-			evt.preventDefault();
-		}
-		if($("#phone").val() == "")
-		{
-			alert("Please enter your phone number")
-			validation =1;
-			evt.preventDefault();
-		}
-		if(validation==0)
-		{
-			var nights = $("#nights").val();
-			if($.isNumeric(nights))
-			{
-				var email = $("#email").val();
-				if(!emailPattern.test(email))
-				{
-					evt.preventDefault();
-					alert("wrong email structure");
-				}
+	//Using the plugging to validate 
+	$("#reservation_form").validate({
+		//establishing the rules for the posible errors in the form
+		rules:{
+			arrival_date:{
+				required:true
+			},
+			nights:{
+				required:true,
+				number:true
+			},
+			name:{
+				required:true
+			},
+			email:{
+				required:true,
+				email: true
+				
+			},
+			phone:{
+				required:true
 			}
-			else
-			{
-				alert("nights value is not a number");
-				evt.preventDefault();
+		},
+		//Establishing the messages for each error
+		messages:{
+			arrival_date:{
+				required:"Arrival date is required"
+			},
+			nights:{
+				required:"Number of Participants is required",
+				number:"This file requires only numbers "
+			},
+			name:{
+				required:"Name is required"
+			},
+			email:{
+				required:"Email is required",
+				email:"This is not a valid email address"
+			},
+			phone:{
+				required:"Phone number is required"
 			}
 		}
 	});
+
 }); // end ready
